f56344b824cba21d0d4ec5a1afee33a7206d6c55
Aiyappa C A <aiyappaca@gmail.com>
2026-02-25 21:07:15 +0530

    Sixth commit

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 from fastapi import FastAPI, Request
 import httpx
+import os
 
 app = FastAPI()
 
-DEBIT_URL = "https://debit-service.onrender.com"
-CARD_URL = "https://card-store-8uzi.onrender.com"
+DEBIT_URL = os.getenv("DEBIT_URL")
+CARD_URL = os.getenv("CARD_URL")
 
-@app.api_route("/api/debit/{path:path}", methods=["GET","POST","PATCH"])
+
+@app.get("/")
+def root():
+    return {"message": "API Gateway Running"}
+
+
+@app.api_route("/api/debit/{path:path}", methods=["GET", "POST", "PATCH"])
 async def proxy_debit(path: str, request: Request):
     async with httpx.AsyncClient() as client:
-        response = await client.request(
-            request.method,
-            f"{DEBIT_URL}/{path}",
-            content=await request.body()
-        )
-    return response.json()
+        try:
+            response = await client.request(
+                request.method,
+                f"{DEBIT_URL}/{path}",
+                content=await request.body(),
+                params=request.query_params,
+                headers=dict(request.headers)
+            )
+            return response.json()
+        except httpx.RequestError:
+            return {"error": "Debit service unavailable"}
 
-@app.api_route("/api/cards/{path:path}", methods=["GET","POST","PUT"])
+
+@app.api_route("/api/cards/{path:path}", methods=["GET", "POST", "PUT"])
 async def proxy_cards(path: str, request: Request):
     async with httpx.AsyncClient() as client:
-        response = await client.request(
-            request.method,
-            f"{CARD_URL}/{path}",
-            content=await request.body()
-        )
-    return response.json()
+        try:
+            response = await client.request(
+                request.method,
+                f"{CARD_URL}/{path}",
+                content=await request.body(),
+                params=request.query_params,
+                headers=dict(request.headers)
+            )
+            return response.json()
+        except httpx.RequestError:
+            return {"error": "Card service unavailable"}
